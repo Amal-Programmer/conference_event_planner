1a2d724732e20327662396839aa0af107c6e54e5
Amal <amal.otmani@usmba.ac.ma>
2025-02-15 00:11:19 +0100

    Finish components

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -37,11 +37,15 @@ export const venueSlice = createSlice({
   
   ],
   reducers: {
-   
+    //action contains the index of the item to be incremented
     incrementQuantity: (state, action) => {
+      //extract the payload property from the action object and rename it to index.
       const { payload: index } = action;
+      //°state is an array of object each object has the properties : img, name, cost and quantity
+    
       if (state[index]) {
         if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
+          //returns early without modifying the state
           return;        }
         state[index].quantity++;
       }
